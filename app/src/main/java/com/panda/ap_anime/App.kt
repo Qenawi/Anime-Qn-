42e92f844a62ff0aa59c8d734d6127ce9be93409
AhmedMohamedHussien <aldmanmenro@yahoo.com>
2019-07-18 09:29:22 -0400

    refactor:ToMVVM Branch

--- app/src/main/java/com/panda/ap_anime/App.kt
+++ app/src/main/java/com/panda/ap_anime/App.kt
@@ -1,11 +1,20 @@
 package com.panda.ap_anime
 
 import android.app.Application
+import org.jetbrains.annotations.NotNull
 import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                return super.createStackElementTag(element) + ":" + element.lineNumber
+            }
+        })
+
     }
 }
